afb65140ed29cae2f48bbe91cffccadadfa77c1c
nao085 <tkn.0805.a@icloud.com>
2019-11-22 18:19:27 +0900

    add-reloadMessage

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -4,7 +4,7 @@ $(function() {
     var messageContent = message.content? message.content : "";
     var messageImage = message.image_url? `<img src="${ message.image_url }">` : ``;
     var html =`
-         <div class="message">
+         <div class="message" data-message-id="${message.id}">
            <div class="message__upper-info">
              <div class="message__upper-info__talker" > ${message.user_name}    
            </div>
@@ -45,4 +45,28 @@ $(function() {
         $('.form__submit').prop('disabled', false);
     });
   });
+
+  var reloadMessages = function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message:last').data("message-id");
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML = buildHTML(message);
+          $('.messages').append(insertHTML);
+        })
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight} , 'fast'); 
+      })
+      .fail(function() {
+        alert('更新に失敗しました');
+      });
+    }
+  };
+  setInterval(reloadMessages, 7000);
 });
